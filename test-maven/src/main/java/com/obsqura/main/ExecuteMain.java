56302e3f2d12cbcced91138afe3e684411e5fa1f
alexobsqura <nithinjames906@gmail.com>
2023-03-10 08:29:55 +0530

    added test-maven project

---
+++ test-maven/src/main/java/com/obsqura/main/ExecuteMain.java
@@ -0,0 +1,16 @@
+package com.obsqura.main;
+
+import java.util.Arrays;
+
+public class ExecuteMain {
+
+	public static void main(String[] args) {
+
+		System.out.println("Running the main method " + args.length);
+
+		if (args.length > 0) {
+			System.out.println("List of arguments: " + Arrays.toString(args));
+		}
+	}
+
+}
